c5388d0681ff760c58a82147de0885012b4fd1fc
Adrian Majcher <majcherrr87@gmail.com>
2023-11-30 14:25:14 +0100

    zmiana nazwy

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
     <div className={styles.container}>
       <header className={styles.header}>
         <div>
-          <h1>Do zrobienia</h1>
+          <h1>ToDo</h1>
           <h2>{getSubHeading(todos.length)}</h2>
         </div>
         {!isFormShown && (
